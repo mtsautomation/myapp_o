c2f657d13004cfcd064c77d3a919c1a6a058069c
Alfredo <motosur.merida@gmail.com>
2025-01-20 20:30:46 -0600

    Fixing shapes, on database

--- myapp.py
+++ myapp.py
@@ -322,11 +322,11 @@ def update_services(df, message_id, date, hour):
         print(num_rows)
         # Check if the DataFrame has more than one row
         if num_rows > 1:
-            print('it is less higher 15')
+
             df = pd.DataFrame(df)
             print("Printing from update multiple rows")
             # Handle multiple-row DataFrame
-            print("DataFrame multiple", df)
+            print("DataFrame multiple", df, type(df))
             s_row = True
             for index, row in df.iterrows():  # Iterate through the rows
                 print("ROW")
